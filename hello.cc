e5432dff80132ff53f9d3a52a3ca7d6db400f5e9
lengstrom <lengstrom@users.noreply.github.com>
2015-07-26 22:48:09 -0400

    moree

--- hello.cc
+++ hello.cc
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <vector>
 
+std::vector<double> return_vec() {
+    std::vector<double> a;
+    a.push_back(1.0);
+    return a;
+}
+
+void a_func(const std::vector<double> a){
+    return;
+}
+
 int main() {
-    double a;
-    std::vector<double>::size_aatype nums = 3;
-    std::cout << nums;
+    const std::vector<double> nums = return_vec();
+    a_func(nums);
+    std::cout << nums[0];
 }
